a2c0fbe5bb18407dc9716f92a517111bb31ed28e
Praneta Dwi Indarti <114267064+PranetaDwi@users.noreply.github.com>
2024-01-02 22:24:58 +0700

    Update channels.php

--- channels.php
+++ channels.php
@@ -21,17 +21,4 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-// Broadcast::channel('chat-message.{userId}', function (User $user, int $receiver_id) {
-//     return $user->id === Chat::findOrNew($receiver_id)->user_id;
-// });
-
-// Broadcast::channel('chat-message.{receiverId}', function ($user, $receiverId) {
-//     return (int) $user->id == (int) $receiverId;
-// });
-
 Broadcast::channel('project-progress.{project_progress_id}', ChatChannel::class);
-
-// Broadcast::channel('project-progress.{project_progress_id}', function ($user, $project_progress_id) {
-//     $projectProgress = ProjectProgress::find($project_progress_id);
-//     return $user->account_id === $projectProgress->buyer_id || $user->account_id === $projectProgress->developer_id;
-// });
